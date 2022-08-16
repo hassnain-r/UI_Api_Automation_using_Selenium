7ff4be9d178a09608b73b7adf232df41f27d83b9
hrehman <hussnain.rehman@arbisoft.com>
2022-08-16 21:11:44 +0500

    final_testing

--- conftest.py
+++ conftest.py
@@ -45,7 +45,6 @@ def setup(request):
     chrome_options.add_argument('--no-sandbox')
     chrome_options.add_argument('--disable-dev-shm-usage')
     driver = webdriver.Chrome(ChromeDriverManager().install(), options=chrome_options)
-    logging.info(os.getenv("BASE_URL"))
     driver.get(os.getenv("BASE_URL"))
     driver.maximize_window()
     request.cls.driver = driver
